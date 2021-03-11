4965ea66420b24d7c850a8ac7207871f6447893f
saikumarkondi <sai@bottlecapps.com>
2021-03-12 01:35:01 +0530

    added basic angular universal with working code

---
+++ src/app/app-config.service.ts
@@ -0,0 +1,8 @@
+import { Injectable} from '@angular/core';
+export const baseURL = 'https://staging.liquorapps.com/Bcapi/api/';
+@Injectable({
+    providedIn: 'root'
+  })
+export class AppConfigService{
+    constructor(){}
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
