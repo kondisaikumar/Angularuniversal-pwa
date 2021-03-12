fae61550e01ec1b5036441a0d07dee7b82bc87e7
saikumarkondi <sai@bottlecapps.com>
2021-03-12 22:55:15 +0530

    browsermodule

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,12 +3,10 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     AppModule,
-    RouterModule,
     ServerModule,
   ],
   bootstrap: [AppComponent],
